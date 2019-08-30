f31f87b8cab73056c8ed9e2ccb342b0f5b192c53
Albi Luzi <albi.luzi@innovaway.al>
2019-08-30 11:25:25 +0200

    Implementing phantomjs as headless browser for testing

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -21,6 +21,10 @@
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 import 'core-js/es7/reflect';
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/string';
+import 'core-js/es6/array';
 
 /**
  * Web Animations `@angular/platform-browser/animations`
